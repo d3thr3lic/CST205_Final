a8e77fd65a60fbafc56ff839ad3cae482becfbe9
Mitchell Saunders <d3thr3lic@gmail.com>
2019-04-18 22:49:48 -0700

    converted sound into a class, added functionality, and added more sounds. Currently working

--- Sound/sound.py
+++ Sound/sound.py
@@ -2,95 +2,138 @@ import time
 
 #mock game so that functions can be defined correctly and globals don't have to be used
 def game():
-  #thie list contains two items, an empty sound, and the time that the sound was started
-    #this will need to be added in the main game function
-  bgMusicInfo = [makeEmptySound(1), 0]
-  
-  startBgMusic(bgMusicInfo)
-  
+  isMusicOn = True
+  gs = GameSounds()
+  if isMusicOn:
+    gs.startMusic()
+    
   #simulates game loop and game actions
   while True:
     userCmd = requestString("What do you want to do next?")
-    continueBgMusic(bgMusicInfo)
-
-
-#SOUND EFFECTS
-#================================================================================================
-#================================================================================================
-#these will be cleaned up more later, this section is a work in progress
-def jumpSound(versionNum):
-  setMediaPathToCurrentDir()
-  trackPath = getMediaPath() + "jump" + str(versionNum) + ".wav"
-  track = makeSound(trackPath)
-  play(track)
-  #explore(track)
-
-def jumpingOnBedSound():
-  setMediaPathToCurrentDir()
-  trackPath = getMediaPath() + "jumping_on_a_bed.wav"
-  track = makeSound(trackPath)
-  play(track)
+    
+    #this should be called every time that game loop comes around to ensure that the music continues
+    #if the user wants it on. It will be on by default every game
+    if isMusicOn:
+      gs.continueMusic()
+    
+    #here are examples of how each of the sound effects are used
+    #as well as how the music start and stop are used
+    if userCmd == "jump":
+      gs.jumpSound()
+    elif userCmd == "jump on bed":
+      gs.jumpingOnBedSound()
+    elif userCmd == "fall":
+      gs.fallSound()
+    elif userCmd == "dance":
+      gs.danceSound()
+    elif userCmd == "run":
+      gs.runningSound()
+    elif userCmd == "cry":
+      gs.cryingSound()
+    elif userCmd == "scream":
+      gs.screamSound()
+    elif userCmd == "laugh":
+      gs.laughingSound()
+    elif userCmd == "jump scare":
+      gs.stopMusic() #maybe stop the music for effect?
+      gs.jumpScare()
+    elif userCmd == "stop music":
+      #if this isn't called, then the music will continue until it finishes
+      gs.stopMusic()
+      isMusicOn = False
+    elif userCmd == "start music" and isMusicOn == False: #checking to see that the music isn't already on will help prevent stacking of music
+      isMusicOn = True
+      #needs to call startMusic() instead of continueMusic() when restarting after stopping
+      #beware of calling this twice, the music will stack up if you do, and stopMusic() wont stop all instances
+      gs.startMusic()
+    elif userCmd == "exit" or userCmd == "quit":
+      gs.stopMusic()
+      break
+    
+class GameSounds:
+  bGTimeStarted = 0
   
-def fallSound(versionNum):
-  setMediaPathToCurrentDir()
-  trackPath = getMediaPath() + "fall" + str(versionNum) + ".wav"
-  track = makeSound(trackPath)
-  play(track)
-
-
-
-#BACKGROUND MUSIC FUNCTIONS
-#================================================================================================
-#================================================================================================
-#call this to initiate the bg music. This should only get called once at the beginning of the game
-def startBgMusic(bgMusicInfo):
-  #sets the media path to the current working dir
-  setMediaPathToCurrentDir()
-  trackPath = getMediaPath() + "jumping_on_a_bed.wav"
-  #sets the bgMusicInfo information
-  bgMusicInfo[0] = makeSound(trackPath)
-  bgMusicInfo[1] = time.time()
-  play(bgMusicInfo[0])
-
-#uses the music info stored in the game to continue the bg music if it is still playing
-def continueBgMusic(bgMusicInfo):
-  if not isSongStillPlaying(bgMusicInfo):
-    startBgMusic(bgMusicInfo)
-
-#checks the song duration and the last time that it was started
-#returns if the song is still playing
-def isSongStillPlaying(bgMusicInfo):
-  songDuration = getDuration(bgMusicInfo[0])
-  now = time.time()
-  timePassed = now - bgMusicInfo[1]
-  if timePassed > songDuration:
-    return False
-  else:
-    return True
-#================================================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#to allow mediaPath to be correct an linux, macos and windows.
-def setMediaPathToCurrentDir():
-  fullPathToFile = os.path.abspath(__file__)
-  if fullPathToFile.startswith('/'):
-    setMediaPath(os.path.dirname(fullPathToFile))
-  else:
-    setMediaPath(os.path.dirname(fullPathToFile) + '\\')
+  def __init__(self, bGTrack = makeEmptySound(1)):
+    self.bGTrack = bGTrack
+    self.setMediaPathToCurrentDir()
+    
+  #SOUND EFFECTS
+  #================================================================================================
+  #================================================================================================
+  #these will be cleaned up more later, this section is a work in progress
+  def jumpSound(self):
+    trackPath = getMediaPath() + "jump.wav"
+    track = makeSound(trackPath)
+    play(track)
+  
+  def jumpingOnBedSound(self):
+    trackPath = getMediaPath() + "jumping_on_a_bed.wav"
+    track = makeSound(trackPath)
+    play(track)
+    
+  def fallSound(self):
+    trackPath = getMediaPath() + "fall.wav"
+    track = makeSound(trackPath)
+    play(track)
+    
+  def jumpScare(self):
+    trackPath = getMediaPath() + "jump_scare.wav"
+    track = makeSound(trackPath)
+    play(track)
+    
+  def danceSound(self):
+    trackPath = getMediaPath() + "dance.wav"
+    track = makeSound(trackPath)
+    play(track)
+    
+  def runningSound(self):
+    trackPath = getMediaPath() + "running.wav"
+    track = makeSound(trackPath)
+    play(track)
+    
+  def cryingSound(self):
+    trackPath = getMediaPath() + "crying.wav"
+    track = makeSound(trackPath)
+    play(track)
+  
+  def screamSound(self):
+    trackPath = getMediaPath() + "scream.wav"
+    track = makeSound(trackPath)
+    play(track)
+   
+  def laughingSound(self):
+    trackPath = getMediaPath() + "laughing.wav"
+    track = makeSound(trackPath)
+    play(track)
+  
+  #BACKGROUND MUSIC FUNCTIONS
+  #================================================================================================
+  #================================================================================================
+  #This will actually do the playing of the music, but doesn't need to be called by game()
+  def startMusic(self):
+    trackPath = getMediaPath() + "detuned_piano.wav"
+    self.bGTrack = makeSound(trackPath)
+    self.bGTimeStarted = time.time()
+    play(self.bGTrack)
+  
+  #checks the song duration and the last time that it was started
+  #then calls the startBgMusic() if is isn't playing any longer
+  #this can be called in lou of calling startBgMusic() completely, even for the first time
+  def continueMusic(self):
+    songDuration = getDuration(self.bGTrack)
+    now = time.time()
+    timePassed = now - self.bGTimeStarted
+    if timePassed > songDuration:
+      self.startMusic()
+      
+  def stopMusic(self):
+    stopPlaying(self.bGTrack)
+  #================================================================================================
+  
+  #to allow mediaPath to be correct an linux, macos and windows.
+  def setMediaPathToCurrentDir(self):
+    fullPathToFile = os.path.abspath(__file__)
+    if fullPathToFile.startswith('/'):
+      setMediaPath(os.path.dirname(fullPathToFile))
+    else:
+      setMediaPath(os.path.dirname(fullPathToFile) + '\\')
